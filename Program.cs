a9d0dfafed8a77a9bb6db5aabd6e1d5f9d37ba1c
Giorgio Della Roscia <giorgio.dellaroscia@we-code.it>
2020-01-16 10:18:56 +0100

    leggo i file da una cartella e creo i relativi csv

--- Program.cs
+++ Program.cs
@@ -7,29 +7,53 @@ using iTextSharp.text.pdf;          //*iTextSharp
 using iTextSharp.text.pdf.parser;   //*iTextSharp Text-Reader
 using System.Collections.Generic;
 using System.Linq;
+using Path = System.IO.Path;
 
 class Pdf2Text
 {
     static void Main(string[] args)
     {
-        // TODO: Specify the URL of your small PDF document (less than 1MB and 10 pages)
-        // To extract text from bigger PDf document, you need to use the async method.
-        string pdfUrl = "file:///C:/Users/Giorgio%20Della%20Roscia/Desktop/1sad.pdf";
+        //Salva in una variabile la lista dei file
+
         Pdf2Text pdfTextExtractor = new Pdf2Text();
-        string text = pdfTextExtractor.extractText(pdfUrl);
+        string text = "";
+        try
+        {
+
+            //Legge tutti i file di una cartella e salva l'indirizzo in una lista
+
+            string[] dirs = Directory.GetFiles(@"C:\\Users\\Giorgio Della Roscia\\Desktop\\Andamento latte\\Progetti\\ExtractTextFromPdfMVC\\PDF");
+            Console.WriteLine($"Number of files: {dirs.Length}.");
+
+            foreach (string path in dirs)
+            {
+                string filename = Path.GetFileName(path).Replace(".pdf","");
+                text = pdfTextExtractor.extractText( filename);
+            }
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("The process failed: {0}", e.ToString());
+        }
+
+        //MessageBox.Show("Stampa la lista dei testi estratti dai file a schermo");
+
         Console.WriteLine("===============================");
         Console.WriteLine("PDF TEXT IS AS FOLLOWS:");
-        Console.WriteLine(text);
+       // Console.WriteLine(text);
         Console.WriteLine("Press Enter to exit.");
         Console.ReadLine();
     }
-    public string extractText(string pdfUrl)
+       
+    
+    //Estrae il testo e lo salva in un file Csv
+    public string extractText(string filename)
     {
         string pdfText = "";
         try  
         {
 
-            String sFilename = "file:///C:/Users/Giorgio%20Della%20Roscia/Desktop/Andamento%20latte/allegati/COSTANTINI_20190304-16.pdf";
+            string sFilename = $"C:\\Users\\Giorgio Della Roscia\\Desktop\\Andamento latte\\Progetti\\ExtractTextFromPdfMVC\\PDF\\{filename}.pdf";
 
             //--< read File >--
 
@@ -40,21 +64,12 @@ class Pdf2Text
                 pdfText = pdfText + PdfTextExtractor.GetTextFromPage(pdf_Reader, i);
             }
 
-
-            List<string> linea = new List<string>();
             //linea = pdftext che diventa una lista
+            List<string> linea = new List<string>();
             linea = pdfText.Split('\n').ToList();
-            //object reader=null;
-            //while (reader.Read())
-            //{
-            //    linea.Add(reader.GetValue(0).ToString()
-            //              + ","
-            //              + reader.GetValue(1).ToString()
-            //              + ","
-            //              + reader.GetValue(2).ToString());
-            //}
-            System.IO.File.WriteAllLines("C:\\Users\\Giorgio Della Roscia\\Desktop\\Andamento latte\\Tabelle\\csv\\ProvaTabellaLatte.csv", linea.ToArray());
-            MessageBox.Show(pdfText);
+
+            System.IO.File.WriteAllLines($"C:\\Users\\Giorgio Della Roscia\\Desktop\\Andamento latte\\Tabelle\\csv\\{filename}.csv", linea.ToArray());
+            //MessageBox.Show(pdfText);
         }
         catch (WebException webEx)
         {
@@ -63,3 +78,47 @@ class Pdf2Text
         return pdfText;
     }
 }
+
+
+
+
+//list<pdfreader> pdfreader0 = new list<pdfreader>();
+//pdfreader0.addrange(sfilename.getfiles.postedfiles.select(f => new pdfreader(f.inputstream)));
+
+//foreach (string path in args)
+//{
+//    if (file.exists(path))
+//    {
+//        // this path is a file
+//        processfile(path);
+//    }
+//    else if (directory.exists(path))
+//    {
+//        // this path is a directory
+//        processdirectory(path);
+//    }
+//    else
+//    {
+//        console.writeline("{0} is not a valid file or directory.", path);
+//    }
+//}
+
+
+//public static void processdirectory(string targetdirectory)
+//{
+//    // process the list of files found in the directory.
+//    string[] fileentries = directory.getfiles(targetdirectory);
+//    foreach (string filename in fileentries)
+//        processfile(filename);
+
+//    // recurse into subdirectories of this directory.
+//    string[] subdirectoryentries = directory.getdirectories(targetdirectory);
+//    foreach (string subdirectory in subdirectoryentries)
+//        processdirectory(subdirectory);
+//}
+
+//// insert logic for processing found files here.
+//public static void processfile(string path)
+//{
+//    console.writeline("processed file '{0}'.", path);
+//}
